63e8284093f4652f4219d6158e82231124af01b4
salehsadid <salehsadid16@gmail.com>
2025-02-24 01:02:57 +0600

    Create sketch_nov28b.ino
    I have added code fro dino bot

---
+++ Dino Bot/sketch_nov28b/sketch_nov28b.ino
@@ -0,0 +1,31 @@
+//Project : Dino Bot
+//Author : Saleh Sadid Mir , Tahmid Hossain Chowdhury Mahin, Eftesamul Hasan Nouman
+
+#include<Servo.h>
+Servo servo1;                            
+
+void setup() {
+
+  pinMode(A0,INPUT);
+  Serial.begin(9600); 
+  
+  servo1.attach(3);
+       
+}
+
+void loop() {
+  int ldr = analogRead(A0);
+  Serial.println(ldr);
+  delay(40);
+  if(ldr<100){
+    delay(71);
+    servo1.write(112);
+    delay(80);
+  } 
+  else {
+
+    servo1.write(70);
+  }
+
+}
+
